316182b4655ffa9c3878d5d6bf97695e774434b5
Vladislav <131907203+VladislavSfrv@users.noreply.github.com>
2026-05-01 09:58:30 +0300

    Update table.js

--- src/components/table.js
+++ src/components/table.js
@@ -8,34 +8,5 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
-
-    before.forEach(item => {    
-        root[item] = cloneTemplate(item);
-        root.container.prepend(root[item].container);
-    })
-
-    after.forEach(item => {    
-        root[item] = cloneTemplate(item);
-        root.container.append(root[item].container);
-    })
-
-    root.container.addEventListener('change', () => {
-        onAction();
-    });
-
-    const render = (data) => {
-
-        const nextRows = data.map(item => {
-            const clone = cloneTemplate(rowTemplate);
-            Object.keys(clone.elements).forEach(key => {
-                const element = clone.elements[key];
-                element.textContent = item[key];
-            }); 
-            return clone.container
-        });
-        root.elements.rows.replaceChildren(...nextRows);
-    }
-    return {...root, render};
+    
 }
